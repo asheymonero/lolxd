120aef1182f3777fb08a07a83218630cff6f5a35
asheymonero <ash@hacari.net>
2025-12-31 18:36:42 -0800

    Update slow-hash.cpp

--- src/crypto/slow-hash.cpp
+++ src/crypto/slow-hash.cpp
@@ -16,45 +16,52 @@ using std::bad_alloc;
 
 namespace Crypto {
 
-  enum {
-    MAP_SIZE = SLOW_HASH_CONTEXT_SIZE + ((-SLOW_HASH_CONTEXT_SIZE) & 0xfff)
-  };
+    enum {
+        MAP_SIZE = SLOW_HASH_CONTEXT_SIZE + ((-SLOW_HASH_CONTEXT_SIZE) & 0xfff)
+    };
 
 #if defined(WIN32)
 
-  cn_context::cn_context() {
-    data = VirtualAlloc(nullptr, MAP_SIZE, MEM_COMMIT, PAGE_READWRITE);
-    if (data == nullptr) {
-      throw bad_alloc();
+    cn_context::cn_context() {
+        data = VirtualAlloc(nullptr, MAP_SIZE, MEM_COMMIT, PAGE_READWRITE);
+        if (data == nullptr) {
+            throw bad_alloc();
+        }
     }
-  }
 
-  cn_context::~cn_context() {
-    if (!VirtualFree(data, 0, MEM_RELEASE)) {
-      throw bad_alloc();
+    cn_context::~cn_context() noexcept {
+        if (data != nullptr) {
+            // Destructors must not throw. If this fails, we just leak instead of terminating.
+            VirtualFree(data, 0, MEM_RELEASE);
+            data = nullptr;
+        }
     }
-  }
 
 #else
 
-  cn_context::cn_context() {
+    cn_context::cn_context() {
 #if !defined(__APPLE__)
-    data = mmap(nullptr, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
+        data = mmap(nullptr, MAP_SIZE, PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
 #else
-    data = mmap(nullptr, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+        data = mmap(nullptr, MAP_SIZE, PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANON, -1, 0);
 #endif
-    if (data == MAP_FAILED) {
-      throw bad_alloc();
+        if (data == MAP_FAILED) {
+            throw bad_alloc();
+        }
+        mlock(data, MAP_SIZE);
     }
-    mlock(data, MAP_SIZE);
-  }
 
-  cn_context::~cn_context() {
-    if (munmap(data, MAP_SIZE) != 0) {
-      throw bad_alloc();
+    cn_context::~cn_context() noexcept {
+        if (data != nullptr && data != MAP_FAILED) {
+            // Destructors must not throw. If this fails, we just leak instead of terminating.
+            // (Optional: munlock(data, MAP_SIZE);)
+            munmap(data, MAP_SIZE);
+            data = nullptr;
+        }
     }
-  }
 
 #endif
 
-}
+} // namespace Crypto
